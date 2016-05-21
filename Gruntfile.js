a1cb11e21abf80728cf4392bb55c03dfe09a74e6
Julián Gutiérrez <juliangut@gmail.com>
2016-05-21 20:56:39 +0200

    update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,21 @@
 'use strict';
 
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  grunt.loadNpmTasks('grunt-composer');
 
-    phplint: {
-      options: {
-        swapPath: '/tmp'
-      },
-      application: [
-        'src/**/*.php',
-        'tests/**/*.php'
-      ]
-    },
-    phpcs: {
-      options: {
-        bin: 'vendor/bin/phpcs',
-        standard: 'PSR2'
-      },
-      application: {
-        dir: [
-          'src',
-          'tests'
-        ]
-      }
-    },
-    phpmd: {
-      options: {
-        bin: 'vendor/bin/phpmd',
-        rulesets: 'unusedcode,naming,design,codesize',
-        reportFormat: 'text'
-      },
-      application: {
-        dir: 'src'
-      }
-    },
-    phpcpd: {
-      options: {
-        bin: 'vendor/bin/phpcpd',
-        quiet: false,
-        ignoreExitCode: true
-      },
-      application: {
-        dir: 'src'
-      }
-    },
-    phpunit: {
-      options: {
-        bin: 'vendor/bin/phpunit',
-          coverage: true
-      },
-      application: {
-        coverageHtml: 'dist/coverage'
-      }
-    },
-    climb: {
-      options: {
-        bin: 'vendor/bin/climb'
-      },
-      application: {
-      }
-    },
-    security_checker: {
-      options: {
-        bin: 'vendor/bin/security-checker',
-        format: 'text'
-      },
-      application: {
-        file: 'composer.lock'
-      }
+  var settings = {
+    config: {
+      src: ['grunt/*.js']
     }
-  });
+  };
+  grunt.initConfig(require('load-grunt-configs')(grunt, settings));
 
   grunt.registerTask('qa', ['phplint', 'phpcs', 'phpmd', 'phpcpd']);
   grunt.registerTask('test', ['phpunit']);
-  grunt.registerTask('security', ['climb', 'security_checker']);
+  grunt.registerTask('security', ['composer:outdated:direct', 'security_checker']);
 
   grunt.registerTask('default', ['qa', 'test']);
 };
